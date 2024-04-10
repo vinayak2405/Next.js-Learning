fe50cead0ceb455c6398ddb8c3766d06598d5be2
vinayak2405 <vinayak24pisolkar@gmail.com>
2024-04-10 10:34:46 +0530

    first lecture

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+
+    return (
+        <>
+            <h1>Indie Gamer</h1>
+            <p>Only the best indie gamer, reviewed for you</p>
+        </>
+    );
+}
