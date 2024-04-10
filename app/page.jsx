3dc811b1b0d39c9b93d274fd3dfa7f6131aa3047
vinayak2405 <vinayak24pisolkar@gmail.com>
2024-04-10 11:30:03 +0530

    page routes (added review and stardrew valley page )

--- app/page.jsx
+++
@@ -1,9 +0,0 @@
-export default function HomePage() {
-
-    return (
-        <>
-            <h1>Indie Gamer</h1>
-            <p>Only the best indie gamer, reviewed for you</p>
-        </>
-    );
-}
